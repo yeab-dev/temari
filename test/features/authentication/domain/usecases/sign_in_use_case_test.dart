ab4bcd6af97f70eec73b170b6a6d0c09d6815e8a
yeabsera <yeab.dev@gmail.com>
2025-03-14 13:01:20 +0300

    feat: add unit tests for an error case
    - Updated sign_in_use_case_test.dart to include tests for error scenarios
    - Updated sign_up_use_case_test.dart to include tests for error scenarios

--- test/features/authentication/domain/usecases/sign_in_use_case_test.dart
+++ test/features/authentication/domain/usecases/sign_in_use_case_test.dart
@@ -1,6 +1,7 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:temari/core/errors/failure.dart';
 import 'package:temari/features/authentication/domain/entities/user.dart';
 import 'package:temari/features/authentication/domain/usecases/sign_in_use_case.dart';
 
@@ -10,22 +11,41 @@ void main() {
   late MockAuthRepo repo;
   late SignInUseCase useCase;
   final tUser = User(id: '1', email: 'user@emailcom');
+  final tFailure = Failure(message: 'Error Occured', statusCode: 123);
   Params params = Params(identifier: 'user@email.com', password: '123');
   setUp(() {
     repo = MockAuthRepo();
     useCase = SignInUseCase(repo: repo);
   });
-
-  test('should call [repo.SignIn] and return with the right data', () async {
-    when(() => repo.signIn(
+  group('signInUseCase', () {
+    test(
+        'should call [repo.SignIn] and return with the right data when successful',
+        () async {
+      when(() => repo.signIn(
+              identifier: any(named: 'identifier'),
+              password: any(named: 'password')))
+          .thenAnswer((_) async => Right(tUser));
+      final result = await useCase(params);
+      expect(result, Right(tUser));
+      verify(() => repo.signIn(
+          identifier: any(named: 'identifier'),
+          password: any(named: 'password'))).called(1);
+      verifyNoMoreInteractions(repo);
+    });
+    test('should call [repo.signIn] and return with error when unseccessful',
+        () async {
+      when(() => repo.signIn(
+              identifier: any(named: 'identifier'),
+              password: any(named: 'password')))
+          .thenAnswer((_) async => Left(tFailure));
+      final result = await useCase(params);
+      expect(result, Left(tFailure));
+      verify(
+        () => repo.signIn(
             identifier: any(named: 'identifier'),
-            password: any(named: 'password')))
-        .thenAnswer((_) async => Right(tUser));
-    final result = await useCase(params);
-    expect(result, Right(tUser));
-    verify(() => repo.signIn(
-        identifier: any(named: 'identifier'),
-        password: any(named: 'password'))).called(1);
-    verifyNoMoreInteractions(repo);
+            password: any(named: 'password')),
+      ).called(1);
+      verifyNoMoreInteractions(repo);
+    });
   });
 }
--- test/features/authentication/domain/usecases/sign_up_use_case_test.dart
+++ test/features/authentication/domain/usecases/sign_up_use_case_test.dart
@@ -1,8 +1,8 @@
 import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
+import 'package:temari/core/errors/failure.dart';
 import 'package:temari/features/authentication/domain/entities/user.dart';
-import 'package:temari/features/authentication/domain/repositories/auth_repo.dart';
 import 'package:temari/features/authentication/domain/usecases/sign_up_use_case.dart';
 
 import 'mock_auth_repo.dart';
@@ -11,21 +11,41 @@ void main() {
   late MockAuthRepo repo;
   late SignUpUseCase useCase;
   final tUser = User(id: '1', email: 'user@email.com');
+  final tFailure = Failure(message: 'Error occured', statusCode: 123);
   Params params = Params(email: 'user@email.com', password: '123');
   setUp(() {
     repo = MockAuthRepo();
     useCase = SignUpUseCase(repo: repo);
   });
 
-  test('should call [repo.signUp] and return with the right data', () async {
-    when(() => repo.signUp(
-            email: any(named: 'email'), password: any(named: 'password')))
-        .thenAnswer((_) async => Right(tUser));
-    final result = await useCase(params);
-    expect(result, Right(tUser));
-    verify(() => repo.signUp(
-        email: any(named: 'email'),
-        password: any(named: 'password'))).called(1);
-    verifyNoMoreInteractions(repo);
+  group('signup usecase', () {
+    test(
+        'should call [repo.signUp] and return with the right data when successfull',
+        () async {
+      when(() => repo.signUp(
+              email: any(named: 'email'), password: any(named: 'password')))
+          .thenAnswer((_) async => Right(tUser));
+      final result = await useCase(params);
+      expect(result, Right(tUser));
+      verify(() => repo.signUp(
+          email: any(named: 'email'),
+          password: any(named: 'password'))).called(1);
+      verifyNoMoreInteractions(repo);
+    });
+    test(
+        'should call [repo.signUp] and return with the Failure when unseccessful',
+        () async {
+      when(
+        () => repo.signUp(
+            email: any(named: 'email'), password: any(named: 'password')),
+      ).thenAnswer((_) async => Left(tFailure));
+      final result = await useCase(params);
+      expect(result, Left(tFailure));
+      verify(
+        () => repo.signUp(
+            email: any(named: 'email'), password: any(named: ('password'))),
+      ).called(1);
+      verifyNoMoreInteractions(repo);
+    });
   });
 }
